abd9d3cc3fd301ce351db4157f5a6cb058b0899a
Brilhante Lua <danielle.alves.carvalho@outlook.com>
2022-02-16 10:45:09 -0300

    219

--- PHP_MySql/twitter_clone/registra_usuario.php
+++ PHP_MySql/twitter_clone/registra_usuario.php
@@ -9,6 +9,32 @@
     $db = new Db();
     $conn = $db->conectaDb();
 
+    $sql = " SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' ";
+
+    if($resultado = mysqli_query($conn, $sql)){
+        $dados_usuario = mysqli_fetch_array($resultado);
+
+        if(isset($dados_usuario["usuario"])){
+            echo 'Usuário já cadastrado';
+            die();
+        }
+    } else {
+        echo "Erro ao tentar localizar o registro";
+    }
+
+    $sql = " SELECT usuario, email FROM usuarios WHERE email = '$email' ";
+
+    if($resultado = mysqli_query($conn, $sql)){
+        $dados_usuario = mysqli_fetch_array($resultado);
+
+        if(isset($dados_usuario["email"])){
+            echo 'E-mail já cadastrado';
+            die();
+        }
+    } else {
+        echo "Erro ao tentar localizar o registro";
+    }
+
     $sql = " INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario','$email','$senha')";
 
     if(mysqli_query($conn, $sql)){
