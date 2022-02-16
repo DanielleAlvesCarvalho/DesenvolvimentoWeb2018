db51ed6df045914d2e516a10b3dfaf58f62a3460
Brilhante Lua <danielle.alves.carvalho@outlook.com>
2022-02-16 10:04:58 -0300

    217

--- PHP_MySql/twitter_clone/registra_usuario.php
+++ PHP_MySql/twitter_clone/registra_usuario.php
@@ -4,7 +4,7 @@
 
     $usuario = $_POST["usuario"];
     $email = $_POST["email"];
-    $senha = $_POST["senha"];
+    $senha = md5($_POST["senha"]);
 
     $db = new Db();
     $conn = $db->conectaDb();
